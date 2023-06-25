bec87f2b44d3696eec7fb050e27a506184619d0c
Aslıhan Yiğit <aslinagi.95@outlook.com>
2023-06-25 22:43:51 +0300

    düzenlemeler

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import scraping
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    scrap = scraping.Scraping("kitapyurdu") #Scraping class örneklendirilir.
+    scrap = scraping.Scraping("kitapsepeti") #Scraping class örneklendirilir.
     scrap.get_data()#Scraping class'ın get_data metodu çağırılır.
--- scraping.py
+++ scraping.py
@@ -2,10 +2,6 @@ from selenium import webdriver
 
 from selenium.webdriver.chrome.service import Service
 
-from selenium.webdriver.support.ui import WebDriverWait
-
-from selenium.webdriver.support import expected_conditions as EC
-
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
@@ -14,12 +10,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from enum import Enum
 
+#from selenium.webdriver.common.by import By
+
+
 
 class web_sites(Enum):
-    kitapyurdu = {'db': 'kitapyurdu', 'url': 'https://www.kitapyurdu.com/kategori/kitap-cocuk-kitaplari/2.html',
+    kitapyurdu = {'collection': 'kitapyurdu', 'url': 'https://www.kitapyurdu.com/index.php?route=product/search&filter_name=python',
                   'main_class': 'product-cr', 'title': 'name', 'publisher': 'publisher', 'author': 'author',
                   'price': 'price'}
-    kitapsepeti = {}
+    kitapsepeti = {'collection': 'kitapsepeti', 'url': 'https://www.kitapsepeti.com/arama?q=python',
+                   'main_class': 'productDetails', 'price': 'discountedPrice'}
 
 
 class Scraping:
@@ -30,25 +30,41 @@ class Scraping:
     def get_data(self):
         client = MongoClient("mongodb://aslihan:123456@localhost:27017/smartmaple")#mongoya bağlanılır.
         db = client['smartmaple']
-        coll = db[self.website['db']]
-        child_books = self.get_child_books()#get_child_books metodu çağırılır.
-        coll.insert_many(child_books)#get_child_books metodundan dönen veriler mongoya yazılır.
+        coll = db[self.website['collection']]
+        books = self.get_python_books()#get_python_books metodu çağırılır.
+        coll.insert_many(books)#get_python_books metodundan dönen veriler mongoya yazılır.
         # cursor = coll.find({})
         # for document in cursor:
         #     print('>>', document, '\n')
         client.close()#bağlantı kapatılır
 
-    def get_child_books(self):
+    def get_python_books(self):
+        import re
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         driver.get(self.website['url'])
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, features="html.parser")
-        child_books = []
+        books = []
         matches = soup.body.find_all("div", {"class": self.website['main_class']})#siteden html bilgilerine göre kazıma yapılır
-        for result in matches: #veriler bir listeye eklenir.
-            book = {'title': result.find("div", {"class": self.website['title']}).get_text().strip(),
-                    'publisher': result.find("div", {"class": self.website['publisher']}).get_text().strip(),
-                    'author': result.find("div", {"class": self.website['author']}).get_text().strip(),
-                    'price': result.find("div", {"class": self.website['price']}).get_text().strip()}
-            child_books.append(book)
-        return child_books
+        if self.website['collection'] == "kitapyurdu":
+            for result in matches: #veriler bir listeye eklenir.
+                price = result.find("div", {"class": self.website['price']}).get_text().strip().replace(',', '.')
+                price = re.findall("\d+\.\d+", price)[0]
+                writer = result.find("div", {"class": self.website['publisher']}).get_text().strip().split("Yazar:")[0]
+                book = {'title': result.find("div", {"class": self.website['title']}).get_text().strip(),
+                        'publisher': writer,
+                        'author': result.find("div", {"class": self.website['author']}).get_text().strip(),
+                        'price': price}
+                books.append(book)
+            return books
+        else:
+            for result in matches: #veriler bir listeye eklenir.
+                data = result.find_all('a')
+                price = result.find("div", {"class": self.website['price']}).get_text().strip().split('\n')[0]
+                book = {'title': data[0].get_text().strip(),
+                        'publisher': data[1].get_text().strip(),
+                        'author': data[2].get_text().strip(),
+                        'price': price}
+                books.append(book)
+            return books
+
